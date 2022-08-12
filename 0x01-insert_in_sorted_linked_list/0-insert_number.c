238c16f121246cea0abbf3ac0e727301c6a85bb2
William05Cardozo <3784@holbertonschool.com>
2022-08-11 22:14:26 -0500

    Task 0: Insert_In_Linked_list and README

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - Function in C that inserts a number
+ * into a sorted singly linked list.
+ * @head: pointer to head of list
+ * @number: number of items to insert_node
+ * Return: listint_t*
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = NULL;
+	listint_t *prev = NULL;
+	listint_t *new;
+	
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+
+	if (*head == NULL || head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
+
+	current = *head;
+	prev = current;
+
+	while (current != NULL)
+	{
+		if (current->n >= number)
+			break;
+		prev = current;
+		current = current->next;
+	}
+	if (prev->n <= number)
+	{
+		prev->next = new;
+		new->next = current;
+	}
+	if (prev->n > number)
+	{
+		*head = new;
+		new->next = prev;
+	}
+	return (*head);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,2 @@
+PROJECT INTERVIEW 0x01-Insert Linked List
+Start: 11/08/2.022 - 9:51pm
